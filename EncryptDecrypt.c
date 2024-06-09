c4f86087f114cff6ddaa6b017841b6ba36d9812a
Anandk0 <109323020+Anandk0@users.noreply.github.com>
2024-06-09 15:34:11 +0530

    Add files via upload

---
+++ EncryptDecrypt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int i, x;
+    char str[100];
+    printf("\nEnter a string:\t");
+    fgets(str,100,stdin);
+    printf("1 = Encrypt the string.\n");
+    printf("2 = Decrypt the string.\n");
+    scanf("%d", &x);
+    switch(x){
+        case 1:for(i=0;(i<100&&str[i]!='\0');i++)
+		str[i]=str[i]+3;
+        	break;
+        case 2:for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        	str[i] = str[i] - 3;
+        	printf("\nDecrypted string: %s\n", str);
+        	break;
+        default:printf("\nError\n");
+    }
+    return 0;
+}
+
+
+
